7f1d4899163be9e35f3f5d9d91f22e5f0bfd2d3d
avinashchopra <avinash.chopra.18@gmail.com>
2022-08-28 23:12:35 -0800

    add changes provided in 23 aug

--- data/js/index.js
+++ data/js/index.js
@@ -2,6 +2,29 @@
  *	 *	Author: Bert De Block
  */
 
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function setCookie(name,value,days) {
+	
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+		alert('setting it all')
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
 $(function()
 {
 	//	elements
@@ -68,3 +91,12 @@ $(function()
 		}});
 	});
 });
+
+window.onload = function () {
+	const cookiesValue = getCookie('showInstruction')
+	if(cookiesValue === null) {
+		$disclaimerWrapper = $('#disclaimerWrapper');
+		$disclaimerWrapper.show().animate({opacity: 1}, {duration: 250, easing: 'easeInOutExpo'});
+		setCookie('showInstruction', '1', 1);
+	}
+}
