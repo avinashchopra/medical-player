7629b5805d06a527f54a2715251b795a9fb17934
avinashchopra <85332351+avinashchopra@users.noreply.github.com>
2022-10-09 21:01:31 -0800

    changes on 9th oct 2022

--- data/js/index.js
+++ data/js/index.js
@@ -20,7 +20,6 @@ function setCookie(name,value,days) {
         var date = new Date();
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
-		alert('setting it all')
     }
     document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
